c9756f048e6f0c01a9af5306864506384e315c65
akira.toyama <toyama.a.ab@gmail.com>
2020-09-19 14:12:29 +0900

    UIPickerViewを追加した。categoryクラスを作成した。 UIPickerViewの詳細を決めていないためエラーが残っている。

---
+++ taskapp/Task.swift
@@ -0,0 +1,41 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 遠山　哲 on 2020/09/17.
+//  Copyright © 2020 tootootooyaman. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+    
+    //カテゴリ
+    let categories = List<Category>()
+    
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+}
+
+class Category: Object {
+    @objc dynamic var id = 0
+    @objc dynamic var categoryname = ""
+    let tasks = LinkingObjects(fromType: Task.self, property: "categories")
+    override static func primaryKey() -> String? {
+    return "id"
+    }
+}
